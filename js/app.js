8c7aa882415f87ae7fd44b5a592ee7e46dbe5095
Viki Zhang <onlyzj@gmail.com>
2017-07-04 23:05:22 +0800

    change Player.sprite to boy char, now all errors in the console is disappeared. but the boy did not appear on the playground.

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(){
-    this.sprite = 'images/char-cat-girl.png';
+    this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function(){
