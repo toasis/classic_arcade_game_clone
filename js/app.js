ecd2640055656df97642c301a86d93278dea8cd2
Viki Zhang <onlyzj@gmail.com>
2017-07-04 23:39:06 +0800

    now two enemies and one player shows on the playground since I add their coorainate.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
+    this.x = x;
+    this.y = y;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -24,25 +26,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(){
+var Player = function(x, y) {
+    "use strict";
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function(){
+Player.prototype.update = function() {
 
 };
 
 Player.prototype.render = function() {
+
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now instantiate your objects.
-var enemy1 = new Enemy();
+var enemy1 = new Enemy(0, 50);
+
+var enemy2 = new Enemy(50, 150);
 
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [enemy1];
+var allEnemies = [enemy1, enemy2];
 // Place the player object in a variable called player
-var player = new Player();
+var player = new Player(200, 375);
 
 
 // This listens for key presses and sends the keys to your
